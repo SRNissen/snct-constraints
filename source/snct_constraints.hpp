6859ae457b30006651f1e0a5da157f93cfdbb4dc
Iye <Iye@fantasymail.de>
2022-10-01 18:34:40 +0200

    Add constraint normalization
    Add constraint normalization such that a set of constraints is reduced to its must narrow intersection.
    
    Converting between two differently constrained objects now also only checks the constraints that are not yet satisfied by the source.

--- source/snct_constraints.hpp
+++ source/snct_constraints.hpp
@@ -90,6 +90,67 @@ namespace snct
 		inline static const char* error_message() noexcept { return "Constraint 'Never_Valid' was violated"; }
 	};
 
+
+	/////////////////////////
+	// traits to define relation between predefined constraints
+	/////////////////////////
+
+    template<auto v1, auto v2>
+    struct constraint_relation< GreaterThan<v1>, Not<v2> >
+    {
+        constexpr static bool narrower = v1 >= v2; //"GreaterThan" is narrower than "Not" when this condition is true
+    };
+
+    template<auto v1, auto v2>
+    struct constraint_relation< LessThan<v1>, Not<v2> >
+    {
+        constexpr static bool narrower = v1 <= v2; 
+    };
+
+    template<auto v1, auto v2>
+    struct constraint_relation< Not<v1>, Not<v2> >
+    {
+        constexpr static bool equivalent = v1 == v2; 
+    };
+
+    template<auto v1, auto v2>
+    struct constraint_relation< GreaterThan<v1>, Minimum<v2> >
+    {
+        constexpr static bool narrower = v1 >= v2; //"GreaterThan" is narrower than "Miniumum" when this condition is true
+    };
+
+    template<auto v1, auto v2>
+    struct constraint_relation< LessThan<v1>, Maximum<v2> >
+    {
+        constexpr static bool narrower = v1 <= v2; //"GreaterThan" is narrower than "Miniumum" when this condition is true
+    };
+
+    template<auto v1, auto v2>
+    struct constraint_relation< GreaterThan<v1>, GreaterThan<v2> >
+    {
+        constexpr static bool equivalent = v1 == v2; //"GreaterThan" is equivalent to "GreaterThan" when this condition is true
+        constexpr static bool narrower = v1 > v2; //"GreaterThan" is narrower than "GreaterThan" when this condition is true
+    };
+
+    template<auto v1, auto v2>
+    struct constraint_relation< LessThan<v1>, LessThan<v2> >
+    {
+        constexpr static bool equivalent = v1 == v2; 
+        constexpr static bool narrower = v1 < v2; 
+    };
+
+	template<auto v1, auto v2>
+	struct constraint_relation< LessThan<v1>, GreaterThan<v2> >
+	{
+		static_assert( v1 >= v2, "The intersection of two constraints should not be empty" );
+	};
+
+	template<auto v1, auto v2>
+	struct constraint_relation< GreaterThan<v1>, LessThan<v2> >
+	{
+		static_assert( v1 <= v2, "The intersection of two constraints should not be empty" );
+	};
+
 }
 
 #endif //header guard
